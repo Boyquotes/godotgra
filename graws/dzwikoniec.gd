87a0df5fd5c7ac3481b9049c38d08a9a2e00e187
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-06-04 21:18:52 +0200

    dopobrania

--- graws/dzwikoniec.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-
-func _on_area_2d_body_entered(body):
-	if "gracz" in body.name:
-		get_tree().change_scene_to_file("res://menu_koniec.tscn")
-		print("kaczkakoniec")
--- graws/lawa.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-
-func _on_lawa_timer_timeout():
-	velocity.y = +1 
-
-
-func _on_lawa_area_2d_body_entered(body):
-	pass 
